d4df07f0cdc5b25ffe57d76ec44bba5911f7878a
janihacker <janieghatel@gmail.com>
2016-08-22 15:10:07 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {no_n4m3},
     admin_users = {},
     disabled_channels = {}
   }
